8a43c1ec0f4085d0daf2ea3a5c81674f3e7ee10b
stefankablowski <stefan.kablowski@gmail.com>
2021-12-20 20:04:08 +0100

    make read and write to json work :+1:

--- macroPi/store.py
+++ macroPi/store.py
@@ -1,15 +1,45 @@
 import jsons as custom_json
-import json
 from serializer import KeySerializer
-from pynput.keyboard import Key, Listener, KeyCode
+from dataclasses import dataclass
 
-def store_key_events(arr):
-    KeySerializer.create_instance()
-    with open('shortcuts.txt','w+') as outfile:
-        for key_event in arr:
-            jsonstr = custom_json.dump(key_event[0]) + '\n'
-            outfile.write(jsonstr)
-            #print(jsonstr)
+CONFIG_FILE_NAME = "shortcuts.json"
+CONFIG_TOP_LEVEL_KEYWORD = "keys"
+CONFIG_REMOTE_SCAN_CODE_KEYWORD = "remote_scan_code"
+CONFIG_KEY_CODE_KEYWORD = "key_code"
+CONFIG_PRESSED_KEYWORD = "pressed"
+CONFIG_RECORDING_KEYWORD = "recording"
 
 def load_key_events():
-    pass
+    with open(CONFIG_FILE_NAME) as config_file:
+        read = config_file.read()
+        json_dict = custom_json.loads(read)
+        config_storage = ConfigStorage(keys={})
+
+        for json_key in json_dict[CONFIG_TOP_LEVEL_KEYWORD].values():
+            
+            config_events = [ConfigEvent(
+                json_record[CONFIG_KEY_CODE_KEYWORD],
+                json_record[CONFIG_PRESSED_KEYWORD]
+            ) for json_record in json_key[CONFIG_RECORDING_KEYWORD]]
+            config_key = ConfigKey(json_key[CONFIG_REMOTE_SCAN_CODE_KEYWORD], config_events)
+            config_storage.keys[config_key.remote_scan_code] = config_key
+    return config_storage
+
+def store_key_events(config_storage):
+    KeySerializer.create_instance()
+    with open(CONFIG_FILE_NAME,'w+') as outfile:     
+        outfile.write(custom_json.dumps(config_storage, {"indent": 4}))
+
+@dataclass
+class ConfigEvent:
+    key_code: str
+    pressed: bool
+
+@dataclass
+class ConfigKey:
+    remote_scan_code: str
+    recording: list[ConfigEvent]
+
+@dataclass
+class ConfigStorage:
+    keys: dict[ConfigKey]
